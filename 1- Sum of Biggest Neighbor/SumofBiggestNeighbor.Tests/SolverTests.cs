eb55696d40431d2ce828be683fb35a16432f48f8
Emad Hosseini <EmadHosseini@hotmail.com>
2020-02-25 21:58:29 +0330

    Written two basic tests

--- 1- Sum of Biggest Neighbor/SumofBiggestNeighbor.Tests/SolverTests.cs
+++ 1- Sum of Biggest Neighbor/SumofBiggestNeighbor.Tests/SolverTests.cs
@@ -5,9 +5,25 @@ namespace SumofBiggestNeighbor.Tests
     public class SolverTests
     {
         [Fact]
-        public void Test1()
+        public void InputSet1_ShouldReturn10()
         {
+            var solver = new Solver();
+            var input = new[] { 1, 2, 1, 5, 5, 3, 3, 3, 4 };
 
+            var result = solver.Challenge(input);
+
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void InputSet2_ShouldReturn12()
+        {
+            var solver = new Solver();
+            var input = new[] { 1, 6, 1, 2, 6, 1, 6, 6 };
+
+            var result = solver.Challenge(input);
+
+            Assert.Equal(12, result);
         }
     }
 }
