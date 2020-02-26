cb3269a72defdf11f3cab1cfea4584353e50d369
Emad Hosseini <EmadHosseini@hotmail.com>
2020-02-26 15:36:45 +0330

    Even more improvement on the alexa

--- 4- Alexa Settings/AlexaSettings/AlexaOptions.cs
+++ 4- Alexa Settings/AlexaSettings/AlexaOptions.cs
@@ -1,13 +1,31 @@
-﻿namespace AlexaSettings
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AlexaSettings
 {
     class AlexaOptions
     {
+        private readonly Dictionary<string, PropertyInfo> formatters;
+
+        public AlexaOptions()
+        {
+            formatters = typeof(AlexaOptions).GetProperties()
+                .Where(p => p.Name != nameof(GreetingMessage))
+                .ToDictionary(p => $"{{{p.Name}}}", p => p);
+        }
+
         public string GreetingMessage { get; set; }
         public string OwnerName { get; set; }
 
         public string GetMessage()
         {
-            return GreetingMessage.Replace("{" + nameof(OwnerName) + "}", OwnerName);
+            var res = GreetingMessage;
+            foreach (var formatter in formatters)
+            {
+                res = res.Replace(formatter.Key, formatter.Value.GetValue(this)?.ToString());
+            }
+            return res;
         }
     }
 }
