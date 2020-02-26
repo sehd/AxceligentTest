bd7c8ec5b553aa7896a476cad0f104b7190b7158
Emad Hosseini <EmadHosseini@hotmail.com>
2020-02-26 10:18:52 +0330

    Minimum viable answer for user class

---
+++ 2- User Class/UserClass/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserClass
+{
+    internal class User
+    {
+        int count = 0;
+
+        internal void Add(string userName)
+        {
+            count++;
+        }
+
+        internal int GetUsersCount()
+        {
+            return count;
+        }
+    }
+}
